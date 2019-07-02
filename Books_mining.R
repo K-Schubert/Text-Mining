ce0cfb65a85f0cfa03a863ec50a9875b0db53aa2
Kieran Schubert <schubert.kieran@gmail.com>
2019-07-02 14:02:47 +0200

    First commit

---
+++ Books_mining.R
@@ -0,0 +1,91 @@
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+library(tidytext)
+library(ggplot2)
+library(gutenbergr)
+library(tidyr)
+library(scales)
+
+
+# Jane Austen books
+austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() -> original_books
+
+original_books
+
+original_books %>%
+  unnest_tokens(word, text) -> tidy_books 
+
+tidy_books
+
+# Remove 'stop words' like "the", "of", "to", etc.
+data(stop_words)
+
+tidy_books %>%
+  anti_join(stop_words) -> tidy_books 
+
+# Count word occurences
+tidy_books %>%
+  count(word, sort = TRUE) 
+
+# Plot most frequent words
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+# H.G. Wells books
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+
+hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) -> tidy_hgwells
+
+tidy_hgwells %>%
+  count(word, sort = TRUE)
+
+# Brontë sisters books
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
+
+bronte %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) -> tidy_bronte
+
+tidy_bronte %>%
+  count(word, sort = TRUE)
+
+# Frequency comparison across books
+bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"), 
+                       mutate(tidy_books, author = "Jane Austen")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  spread(author, proportion) %>% 
+  gather(author, proportion, `Brontë Sisters`:`H.G. Wells`) -> frequency
+
+# Plot frequencies
+# expect a warning about rows with missing values being removed
+ggplot(frequency, aes(x = proportion, y = `Jane Austen`, color = abs(`Jane Austen` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+  facet_wrap(~author, ncol = 2) +
+  theme(legend.position="none") +
+  labs(y = "Jane Austen", x = NULL)
+
